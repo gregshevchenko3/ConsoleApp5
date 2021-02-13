ccdc3bbfd9f1822ca212f6c7c3532fa1809af844
gregshevchenko3 <gregshevchenko3@gmail.com>
2021-02-13 09:53:20 +0200

    Add project files.

---
+++ ConsoleApp5/Library.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Library
+    {
+        private SortedDictionary<char, SortedList<Author, List<Book>>> _library;
+
+        //65-90 A-Z
+        //97-122 a-z
+        //1040-1071
+        //1072-1103
+        public Library()
+        {
+            _library = new SortedDictionary<char, SortedList<Author, List<Book>>>(); //пока пусто!!!
+
+            for (int i = 65; i < 91; i++)
+            {
+                _library.Add((char)i, new SortedList<Author, List<Book>>());
+            }
+            for (int i = 1040; i < 1072; i++)
+            {
+                _library.Add((char)i, new SortedList<Author, List<Book>>());
+            }
+        }
+
+        public void addBook(Book book)
+        {
+            string surname = book.Authors.First<Author>().Surname.ToUpper(); //получаем фамилию первого автора
+            char firstLetter = surname[0]; //первая буква фамилии
+            if (_library.ContainsKey(firstLetter))
+            {
+                //_library[firstLetter] это SortedList<Author, List<Book>>
+                if (!_library[firstLetter].ContainsKey(book.Authors.First<Author>()))
+                {//если уже есть карточка с автором
+                    _library[firstLetter].Add(book.Authors.First<Author>(), new List<Book>());
+                }
+                //добавляем новую книгу нашему автору
+                 _library[firstLetter][book.Authors.First<Author>()].Add(book);
+            }
+        }
+        public List<Book> getAutorWorks(Author author) {
+            throw new Exception();
+        }
+        public Book getBook(Author author, string title)
+        {
+            char symb = author.Surname.ToUpper()[0];
+            if (_library[symb].ContainsKey(author))
+            {//если уже есть карточка с автором
+                foreach (Book book in _library[symb][author])
+                {
+                    if (book.Title.ToLower().Equals(title.ToLower()))
+                    {
+                        return book;
+                    }
+                }
+            }
+            return null;
+        }
+        public List<Book> getBooks(string title)
+        {
+            List<Book> searchBooks = new List<Book>();
+            foreach (var item in _library.Values)
+            {
+                //Console.WriteLine(item.Values); - коллекция книг одного автора
+                foreach (var books in item.Values)
+                {
+                    //Console.WriteLine(books); //список книг одного автора
+                    foreach (var book in books) //получаем доступ к одной книге
+                    {
+                        if (book.Title.Equals(title)) //если совпадает название книги с названием полученным от читателя
+                        {
+                            searchBooks.Add(book);
+                        }
+                    }
+                }
+            }
+            return searchBooks;
+        }
+
+        public Book getBook(string title)
+        {
+            foreach (var item in _library.Values)
+            {
+                //Console.WriteLine(item.Values); - коллекция книг одного автора
+                foreach (var books in item.Values)
+                {
+                    //Console.WriteLine(books); //список книг одного автора
+                    foreach (var book in books) //получаем доступ к одной книге
+                    {
+                        if (book.Title.Equals(title)) //если совпадает название книги с названием полученным от читателя
+                        {
+                            return book;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Author markTven = new Author("Марк", "Твен", new DateTime(1835, 11, 30), Gender.MALE, "Aмериканский писатель, журналист и общественный деятель");
+            Console.WriteLine(markTven);
+
+            foreach (string item in PaperStandarts.allFormats.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (Size item in PaperStandarts.allFormats.Values)
+            {
+                Console.WriteLine($"W: {item.Width}; H: {item.Height}");
+            }
+
+            foreach (KeyValuePair<string, Size> item in PaperStandarts.allFormats)
+            {
+                Console.WriteLine($"Format {item.Key}; Size: W {item.Value.Width}; H {item.Value.Height};");
+            }
+
+            Console.WriteLine(PaperStandarts.getSize("A_0"));
+            Console.WriteLine(PaperStandarts.getPaperFormat("A_0"));
+
+
+            string pathToBook = Environment.CurrentDirectory + "\\" + "book_01.txt";
+            string[] bookRows = FileReader.ReadTxtFile(pathToBook);
+            Console.WriteLine("Колиество строк: " + bookRows.Length);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(bookRows[0]);
+            Console.ResetColor();
+            Console.Read();*/
+
+
+            /*Size sizePaper = PaperStandarts.getSize("A_4");
+            Console.WriteLine($"Height: {sizePaper.Height}; Width: {sizePaper.Width}. ");*/
+
+
+
+            /*List<Page> allPages = 
+            StorageReader.getPages(Environment.CurrentDirectory + "\\" + "book_01.txt", PaperStrogeStandarts.getPaperInfo("A_4"));
+
+            Console.WriteLine(allPages[1].Text);*/
+
+
+            string pathToBook = Environment.CurrentDirectory + "\\" + "book_01.txt";
+            string shortDesc = "современный российский писатель-фантаст. Родился в семье учителя и инженера. До четырнадцати лет жил в Киргизии. Потом переехал на Урал";
+
+            Book book_01 = new Book(pathToBook, "Война и мир", Genres.DRAM, Langs.RU, "A_5", "Noname comp*", new DateTime(2019, 05, 05));
+            book_01.Authors.Add(new Author("Лев", "Толстой", new DateTime(1980, 01, 01), Gender.MALE, shortDesc));
+
+            Book book_02 = new Book(pathToBook, "Зиний вечер", Genres.LOVE_STORY, Langs.RU, "A_5", "Noname comp*", new DateTime(2019, 05, 05));
+            book_02.Authors.Add(new Author("Александр", "Пушкин", new DateTime(1980, 01, 01), Gender.MALE, shortDesc));
+
+            Book book_03 = new Book(pathToBook, "Мертвые души", Genres.DRAM, Langs.RU, "A_5", "Noname comp*", new DateTime(2019, 05, 05));
+            book_03.Authors.Add(new Author("Николай", "Гоголь", new DateTime(1980, 01, 01), Gender.MALE, shortDesc));
+
+            Book book_04 = new Book(pathToBook, "Мертвые души", Genres.DRAM, Langs.RU, "A_5", "Noname comp*", new DateTime(2019, 05, 05));
+            book_04.Authors.Add(new Author("Кто то", "Неизвестный", new DateTime(1980, 01, 01), Gender.MALE, shortDesc));
+
+            Library library = new Library();
+            library.addBook(book_01);
+            library.addBook(book_02);
+            library.addBook(book_03);
+            library.addBook(book_04);
+
+
+            Console.WriteLine("-------------------------");
+            Reader rdr = new Reader("Greg", "Shevchenko", new DateTime(1989, 12, 11), Gender.MALE, "gregshevchenko3@gmail.com", "+380500745280", "38520 Dykanka", ReaderRate.Neutral);
+            Console.WriteLine(rdr);
+            Console.WriteLine("-------------------------");
+
+            Book book_01_ref = library.getBook(book_01.Authors.First<Author>(), "Тайная жизнь города");
+            Console.WriteLine(book_01_ref);
+
+            Console.WriteLine("Нашли книгу: ");
+            Console.WriteLine(library.getBook("Война и мир"));
+            Console.WriteLine("Её жанр: ");
+            Console.WriteLine(library.getBook("Война и мир").Genre);
+
+
+            Console.WriteLine("Поиск нескольких книг по названию: ");
+            Console.WriteLine("Найдено: " + library.getBooks("Мертвые души").Count + "книг");
+
+            ReaderCard card = new ReaderCard(rdr);
+            card.AddRecord(book_03);
+            Console.WriteLine(card);
+
+            card.DelRecord(book_03);
+            Console.WriteLine(card);
+
+            //Library - два метода
+            //реализовать book=>Iprinted
+            //generic collection
+        }
+    }
+}
